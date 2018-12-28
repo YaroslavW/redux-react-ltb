1e193992b4855fb5f72fc4e1b89a76859c1bcfb5
YaroslavW <doctor355@yahoo.com>
2018-12-28 17:12:10 +0300

    for static data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        My App
+        My App {this.props.user}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = store => {
+  return {
+    user: store.user
+  }
+}
+export default connect(mapStateToProps)(App);
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+export const initialState = {
+    user:'unknown user'
+}
+
+export function rootReducer(state = initialState){
+    return state
+}
