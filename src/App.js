eadf175fcdd11bebc33391f4b5d7b7cda9fe45c6
YaroslavW <doctor355@yahoo.com>
2018-12-31 02:55:34 +0300

    User action

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { User } from './components/User';
 import { Page } from './components/Page';
 import { connect } from 'react-redux';
+import { setYear } from './actions/PageActions';
 import './App.css';
 
 class App extends Component {
   render() {
-    const { user, page } = this.props;
+    const { user, page, setYearAction } = this.props;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Мои топ фото</h1>
         </header>
         <User name={ user.name } />
-        <Page photos={ page.photos} year={ page.year } />
+        <Page photos={ page.photos} year={ page.year } setYear={setYearAction}/>
       </div>
     );
   }
@@ -25,4 +26,10 @@ const mapStateToProps = store => {
     page: store.page,
   }
 }
-export default connect(mapStateToProps)(App);
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setYearAction: year => dispatch(setYear(year))
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/Page.js
+++ src/components/Page.js
@@ -2,10 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export class Page extends React.Component {
+    onBtnClick = e =>{
+        const year = e.currentTarget.innerText;
+        this.props.setYear(year);
+    }
     render(){
         const {photos, year } = this.props;
         return(
             <div>
+                <div>
+                    <button onClick={this.onBtnClick}>2018</button>
+                    <button onClick={this.onBtnClick}>2017</button>
+                    <button onClick={this.onBtnClick}>2016</button>
+                    <button onClick={this.onBtnClick}>2015</button>
+                    <button onClick={this.onBtnClick}>2014</button>
+                </div>
                 <p>
                     You have { photos.length } from { year }
                 </p>
@@ -17,4 +28,5 @@ export class Page extends React.Component {
 Page.PropTypes = {
     year: PropTypes.number.isRequired,
     photos: PropTypes.array.isRequired,
+    setYear: PropTypes.func.isRequired,
 }
